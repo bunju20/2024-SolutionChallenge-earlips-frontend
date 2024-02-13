1960204f8b2e0a940abdc28bd2a7ecc32d480089
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-02-13 23:38:34 +0900

    🔨 chore : header widget 주석 추가

--- lib/views/profile/profile_header_widget.dart
+++ lib/views/profile/profile_header_widget.dart
@@ -17,11 +17,10 @@ class ProfileHeader extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            // Welcome or Login Text
+            // 유저 정보가 있으면 이메일을 보여주고, 없으면 로그인 버튼 보여줌
             user != null
                 ? Text("오늘도 이어립스 해볼까요, ${user!.email}")
                 : GestureDetector(
-                    // Make the text clickable
                     onTap: () => Get.to(() => const LoginScreen()),
                     child: const Text(
                       "로그인 하러가기",
