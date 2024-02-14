63db15f0eb9d0525cd36de8a73ea53d7f819bda3
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-02-14 15:49:54 +0900

    ✨feat : 로그인 버튼 수정

--- lib/views/profile/profile_header_widget.dart
+++ lib/views/profile/profile_header_widget.dart
@@ -12,22 +12,25 @@ class ProfileHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(20, 62, 20, 24),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            // 유저 정보가 있으면 이메일을 보여주고, 없으면 로그인 버튼 보여줌
-            user != null
-                ? Text("오늘도 이어립스 해볼까요, ${user!.email}")
-                : GestureDetector(
-                    onTap: () => Get.to(() => LoginScreen()),
-                    child: const Text(
-                      "로그인 하러가기",
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                  ),
-          ],
+      child: GestureDetector(
+        onTap: () => user != null ? null : Get.to(() => LoginScreen()),
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(20, 62, 20, 24),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // 유저 정보가 있으면 이메일을 보여주고, 없으면 로그인 버튼 보여줌
+                user != null
+                    ? Text("오늘도 이어립스 해볼까요, ${user!.email}")
+                    : const Text(
+                        "로그인 하러가기",
+                        style: TextStyle(color: Colors.blue),
+                      ),
+              ],
+            ),
+          ),
         ),
       ),
     );
