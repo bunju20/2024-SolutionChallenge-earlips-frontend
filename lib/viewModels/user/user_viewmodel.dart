65e9266c57770404b0e402e4c8dce85d75cf531b
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-02-16 15:59:10 +0900

    ✨Feat: 유저 뷰모델 언어 - 로컬저장

--- lib/viewModels/user/user_viewmodel.dart
+++ lib/viewModels/user/user_viewmodel.dart
@@ -1,11 +1,13 @@
 // userViewmodel
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 
 class UserViewModel extends GetxController {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final storage = const FlutterSecureStorage(); // Instance for secure storage
 
   // User Profile Data
   final Rx<User?> _firebaseUser = Rx<User?>(null);
@@ -23,6 +25,7 @@ class UserViewModel extends GetxController {
   void onInit() {
     super.onInit();
     _firebaseUser.bindStream(_auth.authStateChanges());
+
     if (uid != null) getUserData();
   }
 
@@ -36,6 +39,19 @@ class UserViewModel extends GetxController {
     }
   }
 
+  void loadLanguageSettings() async {
+    String? storedSystemLanguage = await storage.read(key: 'systemLanguage');
+    String? storedLearningLanguage =
+        await storage.read(key: 'learningLanguage');
+
+    if (storedSystemLanguage != null) {
+      systemLanguage.value = storedSystemLanguage;
+    }
+    if (storedLearningLanguage != null) {
+      learningLanguage.value = storedLearningLanguage;
+    }
+  }
+
 // 언어 설정 업데이트
   Future<void> updateLanguageSettings() async {
     try {
@@ -44,6 +60,31 @@ class UserViewModel extends GetxController {
           'systemLanguage': systemLanguage.value,
           'learningLanguage': learningLanguage.value,
         });
+        // 로컬에도 저장
+        await storage.write(
+          key: 'systemLanguage',
+          value: systemLanguage.value,
+        );
+        await storage.write(
+          key: 'learningLanguage',
+          value: learningLanguage.value,
+        );
+
+        Get.snackbar(
+          "언어 설정 변경",
+          "언어 설정이 변경되었습니다.",
+          snackPosition: SnackPosition.TOP,
+        );
+      } else {
+        // 로컬에도 저장
+        await storage.write(
+          key: 'systemLanguage',
+          value: systemLanguage.value,
+        );
+        await storage.write(
+          key: 'learningLanguage',
+          value: learningLanguage.value,
+        );
         Get.snackbar(
           "언어 설정 변경",
           "언어 설정이 변경되었습니다.",
