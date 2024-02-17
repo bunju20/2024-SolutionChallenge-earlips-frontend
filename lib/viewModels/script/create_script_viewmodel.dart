116bd2c915397f97d7ab4e0767c01a4795fd7250
bunju20 <85238126+bunju20@users.noreply.github.com>
2024-02-17 11:23:22 +0900

    feat: 동영상, 텍스트 특정 api로 전송하는 부분 구현 완

--- lib/viewModels/script/create_script_viewmodel.dart
+++ lib/viewModels/script/create_script_viewmodel.dart
@@ -4,12 +4,15 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:async';
+import 'dart:io';
+import 'package:http/http.dart' as http;
 
 class CreateScriptViewModel extends ChangeNotifier {
   bool isRecording = false;
   bool _isRecorderInitialized = false; // 녹음기 초기화 여부 : 파일
   bool _isRecording = false; // 녹음 중 여부 : 파일
   FlutterSoundRecorder? _audioRecorder;
+  String audioFilePath = '';
 
 
   bool handDone = false;
@@ -41,24 +44,62 @@ class CreateScriptViewModel extends ChangeNotifier {
     if (!_isRecorderInitialized || _isRecording) return;
 
     final directory = await getApplicationDocumentsDirectory();
-    final filePath = '${directory.path}/${DateTime.now().millisecondsSinceEpoch}.aac';
+    final filePath = '${directory.path}/${DateTime
+        .now()
+        .millisecondsSinceEpoch}.aac';
 
     await _audioRecorder!.startRecorder(
       toFile: filePath,
       codec: Codec.aacADTS,
     );
 
-      _isRecording = true;
+    _isRecording = true;
   }
 
-  Future<void> _stopRecording() async {
-    if (!_isRecorderInitialized || !_isRecording) return;
-
-    await _audioRecorder!.stopRecorder();
+  Future<String?> _stopRecording() async {
+    try {
+      final path = await _audioRecorder!.stopRecorder();
+      audioFilePath = path!;
       _isRecording = false;
+      print('Recording stopped, file path: $path');
+      return path; // 녹음이 중지된 파일의 경로를 반환합니다.
+    } catch (e) {
+      print('Error stopping recorder: $e');
+      return null;
+    }
   }
+
+
+  Future<void> sendTextAndAudio() async {
+    String url = 'https://heheds.free.beeceptor.com';
+    String textToSend = writedTextController.text;
+    if (audioFilePath == null) {
+      print('Audio file is not available.');
+      return;
+    }
+
+    try {
+      var request = http.MultipartRequest('POST', Uri.parse(url))
+        ..fields['text'] = textToSend // 텍스트 데이터 추가
+        ..files.add(await http.MultipartFile.fromPath(
+          'audio', // 서버에서 음성 파일을 식별하는 필드명
+          audioFilePath,
+        ));
+
+      var response = await request.send();
+      if (response.statusCode == 200) {
+        print('Data and audio sent successfully');
+      } else {
+        print('Failed to send data and audio');
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+
   void _handleStatus(String status) {
-    if(handDone) return;
+    if (handDone) return;
     if (status == 'done') {
       stopListening();
       Future.delayed(Duration(milliseconds: 100), () {
@@ -75,9 +116,9 @@ class CreateScriptViewModel extends ChangeNotifier {
     }
   }
 
-  void toggleRecording() {
+  void toggleRecording() async {
     isRecording ? stopListening() : startListening();
-    _isRecording ? _stopRecording() : _startRecording();
+    _isRecording ? await _stopRecording() : _startRecording();
     handDone = isRecording;
     isRecording = !isRecording;
     notifyListeners();
@@ -107,19 +148,43 @@ class CreateScriptViewModel extends ChangeNotifier {
   }
 
   void complete() {
-    // 완료 버튼에 대한 로직
     print("완료버튼 눌렀습니다.");
+    sendTextAndAudio(); // 텍스트와 오디오 파일 전송
   }
 
-  @override
-  void dispose() {
-    // 컨트롤러들을 정리합니다.
-    if (_audioRecorder != null) {
-      _audioRecorder!.closeRecorder();
-      _audioRecorder = null;
+
+  Future<void> sendText() async {
+    String url = 'https://heheds.free.beeceptor.com';
+    String textToSend = writedTextController.text;
+
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: {'text': textToSend},
+      );
+
+      if (response.statusCode == 200) {
+        // 요청이 성공적으로 완료되었습니다.
+        print('Data sent successfully');
+      } else {
+        // 서버 오류가 발생했습니다.
+        print('Failed to send data');
+      }
+    } catch (e) {
+      // 네트워크 요청 중 오류가 발생했습니다.
+      print(e.toString());
     }
-    writedTextController.dispose();
-    voicedTextController.dispose();
-    super.dispose();
+
+    @override
+    void dispose() {
+      // 컨트롤러들을 정리합니다.
+      if (_audioRecorder != null) {
+        _audioRecorder!.closeRecorder();
+        _audioRecorder = null;
+      }
+      writedTextController.dispose();
+      voicedTextController.dispose();
+      super.dispose();
+    }
   }
 }
