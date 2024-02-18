11f08cbdd1591fe622fba7d1abc89d65dd1e721d
bunju20 <85238126+bunju20@users.noreply.github.com>
2024-02-18 10:25:13 +0900

    fix: 홈화면 Bottom 블러처리 디자인 수정

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -198,51 +198,54 @@ class _Bottom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          margin: const EdgeInsets.all(20.0),
-          decoration: BoxDecoration(
-            color: const Color(0xFFFFFFFF), // 기본 배경색
-            borderRadius: BorderRadius.circular(30.0),
-          ),
-          child: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 20.0, left: 20.0),
-                alignment: Alignment.centerLeft,
-                child: const Text(
-                  "진행한 학습",
-                  style: TextStyle(
-                    fontFamily: 'Pretendard-Bold',
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+    return Container(
+      width: Get.width * 0.95,
+      margin: const EdgeInsets.only(top: 20.0),
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: const Color(0xFFFFFFFF), // 기본 배경색
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 20.0, left: 20.0),
+                  alignment: Alignment.centerLeft,
+                  child: const Text(
+                    "진행한 학습",
+                    style: TextStyle(
+                      fontFamily: 'Pretendard-Bold',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              const LineChartSample2(),
-            ],
+                const LineChartSample2(),
+              ],
+            ),
           ),
-        ),
-        if (!isLoggedIn) // 로그인 안 됐을 때만 블러 효과와 자물쇠 아이콘 표시
-          Positioned.fill(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(30.0),
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
-                child: Container(
-                  alignment: Alignment.center,
-                  color: Colors.grey.withOpacity(0.1),
-                  child: Icon(
-                    Icons.lock_outline,
-                    size: 60,
-                    color: Colors.white.withOpacity(0.6),
+          if (!isLoggedIn) // 로그인 안 됐을 때만 블러 효과와 자물쇠 아이콘 표시
+            Positioned.fill(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(30.0),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+                  child: Container(
+                    alignment: Alignment.center,
+                    color: Colors.grey.withOpacity(0.1),
+                    child: Icon(
+                      Icons.lock_outline,
+                      size: 60,
+                      color: Colors.white.withOpacity(0.6),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-      ],
+        ],
+      ),
     );
   }
 }
