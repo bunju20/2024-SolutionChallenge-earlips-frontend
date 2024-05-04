b2a123eb626672f32f75996ef6915ea80a6e8bdc
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-05-04 19:28:42 +0900

    ✨Feat: Phoneme Detail Screen

---
+++ lib/views/phoneme/widget/phoneme_list_widget.dart
@@ -0,0 +1,58 @@
+import 'package:earlips/models/phoneme_model.dart';
+import 'package:earlips/models/word_data_model.dart';
+import 'package:earlips/utilities/style/color_system.dart';
+import 'package:earlips/viewModels/word/word_viewmodel.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class PhonemeListWidget extends StatelessWidget {
+  final Phoneme phoneme;
+
+  const PhonemeListWidget({super.key, required this.phoneme});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 8, 20, 0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: ColorSystem.white,
+          border: Border.all(
+            color: const Color(0xffB3CBE2),
+            width: 4,
+          ),
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Column(
+          children: [
+            Container(
+              color: Colors.transparent,
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+            ),
+            ListTile(
+              tileColor: Colors.white,
+              title: Text(
+                phoneme.symbol,
+                style: const TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.w600,
+                  color: ColorSystem.black,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              subtitle: Text(
+                phoneme.symbol,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: ColorSystem.gray4,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
